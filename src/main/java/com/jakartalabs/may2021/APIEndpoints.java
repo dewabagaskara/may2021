772a2a294228f8cd2a2a42be9a8d1e8a890d5a0d
dewabagaskara <dewa_bagaskara@rocketmail.com>
2021-06-25 22:12:11 +0700

    push

--- src/main/java/com/jakartalabs/may2021/APIEndpoints.java
+++ src/main/java/com/jakartalabs/may2021/APIEndpoints.java
@@ -4,5 +4,7 @@ public final class APIEndpoints {
 
 	public final static String loginAPI = "/users/sign_in";
 	public final static String dashboardAPI = "/build_cards";
+	public final static String registerAPI = "/users";
+	public final static String logoutAPI = "/home";
 
 }
